be14f256099d78512ce4e0a6692bd8181fe09810
Alpaca <mihaililyin08@gmail.com>
2024-05-10 22:09:13 +0400

    HalfWorkingEnemies

--- Assets/Scipts/EnemyFollow.cs
+++ Assets/Scipts/EnemyFollow.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using NavMeshPlus.Components;
 using UnityEngine.AI;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class EnemyAI : MonoBehaviour
 {
     [Header("Params")]
     public float speed;
     public float health;
+    public float shootingDistance;
+    public float searchingRadius;
+    public bool SouldSearch;
+    public bool SouldShoot;
+
     [Header("Fields")]
     private NavMeshAgent agent;
     public GameObject SlimeDeathParticles;
+    public LayerMask playerLayer;
+    public GameObject bullet;
     public GameObject target;
+
+    [Header("Debug")]
+    public bool AmIShooting;
+
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -23,11 +35,47 @@ public class EnemyAI : MonoBehaviour
     }
     private void Update()
     {
-        agent.SetDestination(target.transform.position);
+        if (SouldSearch)//Looks for a player in a radius
+        {
+            Collider2D searcher = Physics2D.OverlapCircle(transform.position, searchingRadius, playerLayer);
+            if (searcher)
+            {
+                agent.SetDestination(searcher.transform.position);
+            }
+        }
+        else
+        {
+            agent.SetDestination(target.transform.position);
+        }
+
+
+        if (SouldShoot)//Sends a ray, if it hits smth start a shooting invoke
+        {
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, (target.transform.position-transform.position).normalized, shootingDistance, playerLayer);
+            if (ray)
+            {
+                if (!AmIShooting)
+                {
+                    AmIShooting = true;
+                    InvokeRepeating(nameof(InvokeShoot), 0, 1f);
+                }
+            }
+            else
+            {
+                AmIShooting = false;
+                CancelInvoke(nameof(InvokeShoot));
+            }
+        }
+
+    }
+    public void InvokeShoot()
+    {
+        GameObject blt = Instantiate(bullet, transform.position, Quaternion.identity);
+        blt.GetComponent<Rigidbody2D>().AddForce((target.transform.position-transform.position).normalized * 1000f);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "bullet")
+        if(collision.gameObject.tag == "PlayerBullet")
         {
             health -= collision.gameObject.GetComponent<BulletScript>().damage;
             Destroy(collision.gameObject);
