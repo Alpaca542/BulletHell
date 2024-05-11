6c42d63187b8583c3c495d2f529f9b721430adbd
Ilkka <ilkkp@proton.me>
2024-05-11 15:23:20 +0300

    ObjectPool

---
+++ Assets/Scipts/GameManager.cs
@@ -0,0 +1,48 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private ObjectPool pool = new ObjectPool();
+
+	// Start is called before the first frame update
+    void Start()
+    {
+		LoadPoolablePrefabs();
+	}
+
+	private void LoadPoolablePrefabs()
+	{
+		Object[] objs = Resources.LoadAll("PoolableMonoBehaviours");
+		foreach (Object obj in objs)
+		{
+			if (obj is not GameObject)
+			{
+				Debug.LogError($"Object {obj.name} is not a GameObject");
+				continue;
+			}
+#if UNITY_EDITOR
+			if (UnityEditor.PrefabUtility.GetPrefabAssetType(obj) == UnityEditor.PrefabAssetType.NotAPrefab)
+			{
+				Debug.LogError($"GameObject {obj.name} is not a prefab");
+				continue;
+			}
+#endif
+			GameObject go = (GameObject)obj;
+			Component[] components = go.GetComponents(typeof(Component)).Where(component => component is IPoolable).ToArray();
+			if (components.Length == 0)
+			{
+				Debug.LogError($"Prefab {go.name} does not implement IPoolable");
+				continue;
+			}
+			if (components.Length > 1)
+			{
+				Debug.LogError($"Prefab {go.name} implements IPoolable multiple times");
+				continue;
+			}
+			pool.AddPool(components[0].GetType(), new GameObjectFactory(components[0].gameObject));
+		}
+	}
+}
---
+++ Assets/Scipts/ObjectPool.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public interface IPoolable
+{
+	public void OnGetFromPool();
+	public void OnReturnToPool();
+}
+
+public interface IPoolFactory
+{
+	IPoolable Create();
+}
+
+public class GameObjectFactory : IPoolFactory
+{
+	private readonly GameObject _prefab;
+
+	public GameObjectFactory(GameObject prefab)
+	{
+		_prefab = prefab;
+	}
+
+	public IPoolable Create()
+	{
+		GameObject go = Object.Instantiate(_prefab);
+		Component component = go.GetComponents(typeof(Component)).Where(component => component is IPoolable).ToArray()[0];
+		return (IPoolable)component;
+	}
+}
+
+public class ObjectPool
+{
+	private class Pool
+	{
+		public IPoolFactory factory;
+		public ConcurrentBag<IPoolable> items;
+	}
+
+	public void AddPool(System.Type type, IPoolFactory factory)
+	{
+		_pools.Add(type, new Pool() { factory = factory, items = new ConcurrentBag<IPoolable>() });
+	}
+
+	public IPoolable Get<T>() where T : IPoolable
+	{
+		System.Type type = typeof(T);
+		IPoolable item;
+		if (!_pools[type].items.TryTake(out item))
+			item = _pools[type].factory.Create();
+		item.OnGetFromPool();
+		return item;
+	}
+
+	public void Return<T>(T item) where T : IPoolable
+	{
+		item.OnReturnToPool();
+		_pools[typeof(T)].items.Add(item);
+	}
+
+	private Dictionary<System.Type, Pool> _pools = new Dictionary<System.Type, Pool>();
+}
