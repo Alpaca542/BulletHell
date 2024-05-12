6df4db43b3829b73682f073c089b5db63c6c96e5
Alpaca <mihaililyin08@gmail.com>
2024-05-12 20:18:35 +0400

    Fixes

--- Assets/Scipts/GunScript.cs
+++ Assets/Scipts/GunScript.cs
@@ -39,9 +39,9 @@ public class GunScript : MonoBehaviour
         }
         if (Input.GetMouseButtonDown(1))
         {
-            if(angleOfChange != 90)
+            if(angleOfChange != 180)
             {
-                angleOfChange = angleOfChange + 45f;
+                angleOfChange = angleOfChange + 30f;
             }
             else
             {
@@ -58,7 +58,7 @@ public class GunScript : MonoBehaviour
         GameObject blt2 = ((BulletScript)GameManager.Instance.pool.Get<BulletScript>()).gameObject;
         blt2.GetComponent<BulletScript>().AmIFromPlayer = true;
         blt2.transform.localScale = new Vector3(0.03f, 0.03f, 1f);
-        blt2.transform.localPosition = new Vector2(transform.position.x + 0.5f, transform.position.y - 0.5f);
+        blt2.transform.localPosition = new Vector2(transform.position.x + 0.5f, transform.position.y - 0.2f);
         blt2.transform.rotation = Quaternion.Euler(new Vector3(0, 0, plr.transform.rotation.z));
         blt2.transform.Rotate(new Vector3(0, 0, angleOfChange));
         blt2.GetComponent<Rigidbody2D>().AddForce(blt2.transform.up * 1000f);
@@ -66,8 +66,8 @@ public class GunScript : MonoBehaviour
         GameObject blt3 = ((BulletScript)GameManager.Instance.pool.Get<BulletScript>()).gameObject;
         blt3.GetComponent<BulletScript>().AmIFromPlayer = true;
         blt3.transform.localScale = new Vector3(0.03f, 0.03f, 1f);
-        blt3.transform.localPosition = new Vector2(transform.position.x - 0.5f, transform.position.y-0.5f);
-        blt3.transform.rotation = plr.transform.rotation;
+        blt3.transform.localPosition = new Vector2(transform.position.x - 0.5f, transform.position.y-0.2f);
+        blt2.transform.rotation = Quaternion.Euler(new Vector3(0, 0, plr.transform.rotation.z));
         blt3.transform.Rotate(new Vector3(0, 0, -angleOfChange));
         blt3.GetComponent<Rigidbody2D>().AddForce(blt3.transform.up * 1000f);
     }
