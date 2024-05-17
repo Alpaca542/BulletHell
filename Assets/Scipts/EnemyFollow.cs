d97fc23ebee24f62054ecff020c874ab72679701
Alpaca <mihaililyin08@gmail.com>
2024-05-17 15:29:57 +0400

    DialoguesAndStuff

--- Assets/Scipts/EnemyFollow.cs
+++ Assets/Scipts/EnemyFollow.cs
@@ -164,22 +164,45 @@ public class EnemyAI : MonoBehaviour
         {
             if (target != null)
             {
-                if (SouldGoUp)
+                if(target.transform.tag == "Boss")
                 {
-                    agent.SetDestination(new Vector2(target.transform.position.x, target.transform.position.y + distance2));
-                }
-                else if (ShouldGoSide)
-                {
-                    agent.SetDestination(new Vector2(target.transform.position.x + distance2, target.transform.position.y));
-                }
-                else if (ShouldGoDiag)
-                {
-                    agent.SetDestination(new Vector2(target.transform.position.x + distance, target.transform.position.y + distance2));
+                    if (SouldGoUp)
+                    {
+                        agent.SetDestination(new Vector2(target.transform.position.x, target.transform.position.y + distance2 + 5f));
+                    }
+                    else if (ShouldGoSide)
+                    {
+                        agent.SetDestination(new Vector2(target.transform.position.x + distance2 + 5f, target.transform.position.y));
+                    }
+                    else if (ShouldGoDiag)
+                    {
+                        agent.SetDestination(new Vector2(target.transform.position.x + distance * 1.5f, target.transform.position.y + distance2 + 5f));
+                    }
+                    else
+                    {
+                        agent.SetDestination(new Vector2(target.transform.position.x + distance + 5f, target.transform.position.y + distance * 1.5f));
+                    }
                 }
                 else
                 {
-                    agent.SetDestination(new Vector2(target.transform.position.x + distance, target.transform.position.y + distance));
+                    if (SouldGoUp)
+                    {
+                        agent.SetDestination(new Vector2(target.transform.position.x, target.transform.position.y + distance2));
+                    }
+                    else if (ShouldGoSide)
+                    {
+                        agent.SetDestination(new Vector2(target.transform.position.x + distance2, target.transform.position.y));
+                    }
+                    else if (ShouldGoDiag)
+                    {
+                        agent.SetDestination(new Vector2(target.transform.position.x + distance, target.transform.position.y + distance2));
+                    }
+                    else
+                    {
+                        agent.SetDestination(new Vector2(target.transform.position.x + distance, target.transform.position.y + distance));
+                    }
                 }
+
             }
         }
 
